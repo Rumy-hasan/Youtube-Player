fc78ee03114dd25afa82d83bca04509a3382de1b
Rumy-Hasan <md.rejaulhasanrumy@gmail.com>
2019-06-17 11:48:40 +0600

    some refactoring of main view controller

--- videoTest/ViewController.swift
+++ videoTest/ViewController.swift
@@ -11,23 +11,14 @@ import AVKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    
-    var playerController = AVPlayerViewController()
-    var player: AVPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
-        let movieURL = URL(string: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
-        if let url = movieURL {
-            self.player = AVPlayer(url: url)
-            self.playerController.player = self.player
-        }
     }
 
     @IBAction func playVideo(_ sender: AnyObject) {
         let a = videoLaunch()
         a.showVideoPlayer()
     }
-
 }
 
 
